483dd7da8f7790d5d43fe25f48160b2bb4fe6e3b
McShinyShoe <mcshinyshoe@gmail.com>
2024-06-04 17:44:19 +0700

    fix: fixed const front and back needing an argument even tho they dont

--- include/shiny/array.hpp
+++ include/shiny/array.hpp
@@ -4,7 +4,7 @@
 namespace shiny {
     template <typename t_type, size_t t_size> class Array {
       public:
-        typedef t_type data_type;
+        typedef t_type value_type;
 
       private:
         t_type m_data[t_size];
@@ -16,9 +16,9 @@ namespace shiny {
         t_type &at(size_t index) { return m_data[index]; }
         const t_type &at(size_t index) const { return m_data[index]; }
         t_type &front() { return at(0); }
-        const t_type &front(size_t index) const { at(0); }
+        const t_type &front() const { at(0); }
         t_type &back() { return at(t_size - 1); }
-        const t_type &back(size_t index) const { at(t_size - 1); }
+        const t_type &back() const { at(t_size - 1); }
         template <size_t index> inline const t_type &get() const { return m_data[index]; }
         template <size_t index> inline void set(const t_type &data) { m_data[index] = data; }
         void fill(const t_type &data) { std::fill(m_data, m_data + t_size, data); }
