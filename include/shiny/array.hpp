5905465999435b15babf9087b534582b3c06bca8
McShinyShoe <mcshinyshoe@gmail.com>
2024-06-04 16:41:56 +0700

    feat: moved include files to shiny directory

--- include/shiny/array.hpp
+++ include/shiny/array.hpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <cstddef>
-
+// LinearData
 namespace shiny {
     template <typename t_type, size_t t_size> class Array {
       private:
@@ -8,6 +8,7 @@ namespace shiny {
 
       public:
         // Class Methodes
+        typedef t_type data_type;
         void set(const t_type &data, size_t index) { m_data[index] = m_data; }
         const t_type &get(size_t index) const { return m_data[index]; }
         template <size_t index> inline const t_type &get() const { return m_data[index]; }
@@ -15,8 +16,9 @@ namespace shiny {
         void copy(const t_type *array) { std::copy(array, array + t_size, m_data); }
         t_type *c_arr() { return m_data; }
         const t_type *c_arr() const { return m_data; }
+        size_t size() { return t_size; };
 
-        // Iterator
+        // Iterator>
         t_type *begin() { return m_data; }
         const t_type *begin() const { return m_data; }
         t_type *end() { return m_data + t_size; }
