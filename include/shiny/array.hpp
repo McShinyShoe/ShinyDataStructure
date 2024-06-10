c1061375d1fdea23f8f8a3bf41cdcb05dc7d4580
McShinyShoe <mcshinyshoe@gmail.com>
2024-06-10 13:53:31 +0700

    chore: reorganize Array functions

--- include/shiny/array.hpp
+++ include/shiny/array.hpp
@@ -10,14 +10,11 @@ namespace shiny {
         t_type m_data[t_size];
 
       public:
-        // Class Methodes
-        inline void set(const t_type &data, size_t index) { m_data[index] = m_data; }
-        template <size_t index> inline void set(const t_type &data) { m_data[index] = data; }
+        inline t_type *c_arr() { return m_data; }
         inline t_type &at(size_t index) { return m_data[index]; }
         inline t_type &front() { return at(0); }
         inline t_type &back() { return at(t_size - 1); }
-        inline t_type *c_arr() { return m_data; }
-        template <size_t index> inline const t_type &get() const { return m_data[index]; }
+        inline void set(const t_type &data, size_t index) { m_data[index] = m_data; }
         inline const t_type &get(size_t index) const { return m_data[index]; }
         inline const t_type &at(size_t index) const { return m_data[index]; }
         inline const t_type &front() const { at(0); }
@@ -27,8 +24,9 @@ namespace shiny {
         inline void copy(const t_type *array) { std::copy(array, array + t_size, m_data); }
         inline size_t size() { return t_size; };
         inline bool empty() { return size() == 0; };
+        template <size_t index> inline void set(const t_type &data) { m_data[index] = data; }
+        template <size_t index> inline const t_type &get() const { return m_data[index]; }
 
-        // Iterator>
         inline t_type *begin() { return m_data; }
         inline const t_type *begin() const { return m_data; }
         inline t_type *end() { return m_data + t_size; }
