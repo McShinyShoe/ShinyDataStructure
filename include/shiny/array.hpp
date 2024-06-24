0858c2b9b5121ca0e7a7070cc902fa22151f655d
McShinyShoe <mcshinyshoe@gmail.com>
2024-06-24 22:35:32 +0700

    chore: reorganize Array functions

--- include/shiny/array.hpp
+++ include/shiny/array.hpp
@@ -10,7 +10,7 @@ namespace shiny {
         t_type m_data[t_size];
 
       public:
-        inline t_type *c_arr() { return m_data; }
+        // Linear Data Structure Methodes
         inline t_type &at(size_t index) { return m_data[index]; }
         inline t_type &front() { return at(0); }
         inline t_type &back() { return at(size() - 1); }
@@ -19,14 +19,20 @@ namespace shiny {
         inline const t_type &at(size_t index) const { return at(index); }
         inline const t_type &front() const { return front(); }
         inline const t_type &back() const { return back(); }
-        inline const t_type *c_arr() const { return c_arr(); }
-        inline void fill(const t_type &data) { std::fill(m_data, m_data + t_size, data); }
-        inline void copy(const t_type *array) { std::copy(array, array + t_size, m_data); }
         inline size_t size() { return t_size; };
         inline bool empty() { return size() == 0; };
+
+        // Templated Linear Data Structure Methodes
         template <size_t index> inline void set(const t_type &data) { at(index) = data; }
         template <size_t index> inline const t_type &get() const { return at(index); }
 
+        // Array Methodes
+        inline t_type *c_arr() { return m_data; }
+        inline const t_type *c_arr() const { return c_arr(); }
+        inline void copy(const t_type *array) { std::copy(array, array + t_size, m_data); }
+        inline void fill(const t_type &data) { std::fill(m_data, m_data + t_size, data); }
+
+        // Iterator Methodes
         inline t_type *begin() { return m_data; }
         inline const t_type *begin() const { return m_data; }
         inline t_type *end() { return m_data + t_size; }
