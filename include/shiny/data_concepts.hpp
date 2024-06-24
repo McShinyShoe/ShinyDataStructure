1fca866b8e0a980a1f6c98d63d6fc3d762d8de01
McShinyShoe <mcshinyshoe@gmail.com>
2024-06-24 22:30:44 +0700

    feat: added requirement for LinearDataStructure concept when data structure is const

--- include/shiny/data_concepts.hpp
+++ include/shiny/data_concepts.hpp
@@ -3,15 +3,18 @@
 
 namespace shiny {
     template <typename DataStructure>
-    concept LinearDataStructure = requires(DataStructure dataStructure, size_t index, typename DataStructure::value_type data) {
+    concept LinearDataStructure = requires(const DataStructure &constDataStructure, DataStructure &dataStructure, size_t index, typename DataStructure::value_type data) {
         typename DataStructure::value_type;
-        { dataStructure.at(index) } -> std::convertible_to<typename DataStructure::value_type>;
-        { dataStructure.front() } -> std::convertible_to<typename DataStructure::value_type>;
-        { dataStructure.back() } -> std::convertible_to<typename DataStructure::value_type>;
+        { dataStructure.at(index) } -> std::convertible_to<typename DataStructure::value_type &>;
+        { constDataStructure.at(index) } -> std::convertible_to<const typename DataStructure::value_type>;
+        { dataStructure.front() } -> std::convertible_to<typename DataStructure::value_type &>;
+        { constDataStructure.front() } -> std::convertible_to<const typename DataStructure::value_type>;
+        { dataStructure.back() } -> std::convertible_to<typename DataStructure::value_type &>;
+        { constDataStructure.back() } -> std::convertible_to<const typename DataStructure::value_type>;
         { dataStructure.set(data, index) } -> std::same_as<void>;
-        { dataStructure.get(index) } -> std::convertible_to<typename DataStructure::value_type>;
+        { constDataStructure.get(index) } -> std::convertible_to<typename DataStructure::value_type>;
         { dataStructure.fill(data) } -> std::same_as<void>;
-        { dataStructure.size() } -> std::convertible_to<size_t>;
-        { dataStructure.empty() } -> std::same_as<bool>;
+        { constDataStructure.size() } -> std::convertible_to<size_t>;
+        { constDataStructure.empty() } -> std::same_as<bool>;
     };
 }
